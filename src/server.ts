198538373bbca16d4a3af129bb7f5f0728413a1d
Guilherme Henrique <guilherhenri12@gmail.com>
2025-06-07 16:30:26 -0300

    feat(api): create basic express server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,22 @@
-console.log('Hello World')
+import cors from 'cors'
+import express, { Express, Request, Response } from 'express'
+
+const app: Express = express()
+const port: number = 3000
+
+// Enable CORS for all origins
+app.use(
+  cors({
+    origin: '*',
+  }),
+)
+
+// Basic route
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, TypeScript Express Server!')
+})
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`)
+})
