6cf501cd54ffb106f0bfbd3f35469e5c3e013107
Guilherme Henrique <guilherhenri12@gmail.com>
2025-06-09 11:55:24 -0300

    feat(users): add POST /users route
    - Registers the `createUser` controller to handle user creation.
    - Configures the new endpoint `/users` within the main router.

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 
 import { createBook } from './controllers/create-book'
+import { createUser } from './controllers/create-user'
 import { deleteBook } from './controllers/delete-book'
 import { getBook } from './controllers/get-book'
 import { listBooks } from './controllers/list-books'
@@ -15,3 +16,5 @@ router.get('/books/:id', getBook)
 router.post('/books', createBook)
 router.put('/books/:id', updateBook)
 router.delete('/books/:id', deleteBook)
+
+router.post('/users', createUser)
