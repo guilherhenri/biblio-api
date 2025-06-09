4b913c3102ec42acb250ced6fdf3a3c8cf1d7b65
Guilherme Henrique <guilherhenri12@gmail.com>
2025-06-09 12:49:12 -0300

    feat(loans): add loan registration controller
    - Implements `registerLoan` controller for creating new loan records.
    - Validates `userId` and `bookId` from request body using Zod.
    - Checks for existence of both user and book; returns 404 if not found.
    - Verifies book availability; returns 409 if book is not available.
    - Prevents multiple active loans for the same user and book; returns 409.
    - Creates loan record and updates book's `isAvailable` status to false.
    - Returns `201 Created` on successful loan registration.

---
+++ src/http/controllers/register-loan.ts
@@ -0,0 +1,57 @@
+import { Request, Response } from 'express'
+import { z } from 'zod'
+
+import { CustomError } from '@/core/errors/custom-error'
+import { Book } from '@/infra/database/models/book'
+import Loan from '@/infra/database/models/loan'
+import { User } from '@/infra/database/models/user'
+
+export async function registerLoan(req: Request, res: Response) {
+  const registerLoanBodySchema = z.object({
+    userId: z.string(),
+    bookId: z.string(),
+  })
+
+  const { userId, bookId } = registerLoanBodySchema.parse(req.body)
+
+  const userExists = await User.findByPk(userId)
+
+  if (!userExists) {
+    throw new CustomError('User not found', 404)
+  }
+
+  const bookExists = await Book.findByPk(bookId)
+
+  if (!bookExists) {
+    throw new CustomError('Book not found', 404)
+  }
+
+  if (!bookExists.getDataValue('isAvailable')) {
+    throw new CustomError('Book is not available', 409)
+  }
+
+  const userAlreadyBorrowedTheBook = await Loan.findOne({
+    where: {
+      userId,
+      bookId,
+      giveBack: null,
+    },
+  })
+
+  if (userAlreadyBorrowedTheBook) {
+    throw new CustomError('User has already borrowed the book', 409)
+  }
+
+  const loan = await Loan.create({
+    userId,
+    bookId,
+  })
+
+  await bookExists.update({
+    isAvailable: false,
+  })
+
+  res.status(201).send({
+    loan,
+  })
+}
