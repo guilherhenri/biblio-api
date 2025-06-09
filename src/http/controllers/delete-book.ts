c1be4db91b16b85eacefd1f10d9d795565b6d8d6
Guilherme Henrique <guilherhenri12@gmail.com>
2025-06-09 09:23:59 -0300

    feat(books): add controller to delete a book
    - Implements `deleteBook` controller to remove book records.
    - Validates `id` parameter from request params using Zod.
    - Ensures book exists before deletion; throws 404 if not found.
    - Deletes the specified book from the database.

---
+++ src/http/controllers/delete-book.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express'
+import { z } from 'zod'
+
+import { CustomError } from '@/core/errors/custom-error'
+import Book from '@/infra/database/models/book'
+
+export async function deleteBook(req: Request, res: Response) {
+  const deleteBookParamsSchema = z.object({
+    id: z.coerce.number(),
+  })
+
+  const { id } = deleteBookParamsSchema.parse(req.params)
+
+  const book = await Book.findByPk(id)
+
+  if (!book) {
+    throw new CustomError('Book not found', 404)
+  }
+
+  await book.destroy()
+
+  res.send()
+}
