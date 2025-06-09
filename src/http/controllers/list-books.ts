0b52d3c1faab36c4ff67a83739c502a3860465c9
Guilherme Henrique <guilherhenri12@gmail.com>
2025-06-09 08:41:48 -0300

    feat(books): add controller for listing books
    - Implement `listBooks` controller to fetch all book records.
    - Utilize `Book.findAll()` to retrieve data from the database.
    - Send the retrieved list of books in the response body.

---
+++ src/http/controllers/list-books.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express'
+
+import Book from '@/infra/database/models/book'
+
+export async function listBooks(req: Request, res: Response) {
+  const books = await Book.findAll()
+
+  res.send({
+    books,
+  })
+}
