d678589a3d6e0297dcfb1afe3008195cd3ebc7bf
Guilherme Henrique <guilherhenri12@gmail.com>
2025-06-09 12:14:53 -0300

    feat(users): add GET /users route
    - Registers the `listUsers` controller to handle GET requests.
    - Configures the new endpoint `/users` within the main router.

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -5,6 +5,7 @@ import { createUser } from './controllers/create-user'
 import { deleteBook } from './controllers/delete-book'
 import { getBook } from './controllers/get-book'
 import { listBooks } from './controllers/list-books'
+import { listUsers } from './controllers/list-users'
 import { updateBook } from './controllers/update-book'
 
 export const router = Router({
@@ -17,4 +18,5 @@ router.post('/books', createBook)
 router.put('/books/:id', updateBook)
 router.delete('/books/:id', deleteBook)
 
+router.get('/users', listUsers)
 router.post('/users', createUser)
