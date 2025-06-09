8bfc0bbfe74044d606ebad7fba6251ddf1bea662
Guilherme Henrique <guilherhenri12@gmail.com>
2025-06-09 08:22:04 -0300

    feat(books): add book creation endpoint with validations
    - Implement POST /books endpoint for
    creating new books.
    - Validate request body using Zod for `title`,
    `author`, `ISBN` (13 characters), and
    `year` (4 characters).
    - Enforce business rule:
    ISBN must be unique in the database.
    - Enforce business rule:
    Book year must be a past or current year.
    - Return HTTP status 201 on successful
    book creation.
    - Utilize `CustomError` for handling validation
    and business rule violations.
    
    Close N/A

---
+++ src/http/controllers/create-book.ts
@@ -0,0 +1,43 @@
+import { Request, Response } from 'express'
+import { z } from 'zod'
+
+import { CustomError } from '@/core/errors/custom-error'
+import Book from '@/infra/database/models/book'
+
+export async function createBook(req: Request, res: Response) {
+  const createBookBodySchema = z.object({
+    title: z.string(),
+    author: z.string(),
+    ISBN: z.string().length(13),
+    year: z.string().length(4),
+  })
+
+  const { title, author, ISBN, year } = createBookBodySchema.parse(req.body)
+
+  const isISBNAlreadyRegister = await Book.findOne({
+    where: {
+      title,
+    },
+  })
+
+  if (isISBNAlreadyRegister) {
+    throw new CustomError('ISBN already register', 409)
+  }
+
+  const currentYear = new Date().getFullYear()
+
+  if (currentYear < Number(year)) {
+    throw new CustomError('Year need to be a past date', 409)
+  }
+
+  const book = await Book.create({
+    title,
+    author,
+    ISBN,
+    year,
+  })
+
+  res.status(201).send({
+    book,
+  })
+}
